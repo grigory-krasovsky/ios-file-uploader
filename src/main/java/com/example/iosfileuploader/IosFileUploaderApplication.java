15ff83278e925560d9aad8c5b99375f5e55de047
krasovsky <krasovsky@elbrus.ru>
2025-05-12 19:16:30 +0300

    initial commit

---
+++ src/main/java/com/example/iosfileuploader/IosFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.iosfileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosFileUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IosFileUploaderApplication.class, args);
+    }
+
+}
